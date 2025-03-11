0933ea9830d0070be7005dcfe75f4c0f0cd7a07c
Tanvir Anjum Neon <taneon98@gmail.com>
2025-03-11 22:43:36 +0600

    police job

--- [qb]/qb-policejob/locales/en.lua
+++ [qb]/qb-policejob/locales/en.lua
@@ -26,7 +26,7 @@ local Translations = {
         no_driver_license = 'No drivers license',
         not_cuffed_dead = 'Civilian isn\'t cuffed or dead',
         fine_yourself = 'You Cannot Fine Yourself',
-        not_online = 'This person is not online'
+        not_online = "This person is not online"
     },
     success = {
         uncuffed = 'You have been uncuffed',
@@ -43,7 +43,7 @@ local Translations = {
         vehicle_flagged = 'Vehicle %{plate} has been flagged for %{reason}',
         impound_vehicle_removed = 'Vehicle taken out of impound!',
         impounded = 'Vehicle impounded',
-    },
+ },
     info = {
         mr = 'Mr.',
         mrs = 'Mrs.',
@@ -63,7 +63,6 @@ local Translations = {
         cuff = 'You are cuffed!',
         cuffed_walk = 'You are cuffed, but you can walk',
         vehicle_flagged = 'Vehicle %{vehicle} is flagged for: %{reason}',
-        flagged_vehicle_radar = 'Flagged vehicle spotted by radar: %{plate}',
         unflag_vehicle = 'Vehicle %{vehicle} is unflagged',
         tow_driver_paid = 'You paid the tow truck driver',
         paid_lawyer = 'You paid a lawyer',
@@ -85,7 +84,6 @@ local Translations = {
         amount = 'Amount',
         police_plate = 'LSPD', --Should only be 4 characters long
         vehicle_info = 'Engine: %{value} % | Fuel: %{value2} %',
-        evidence_stash_prompt = 'Evidence Stash',
         evidence_stash = 'Evidence Stash | %{value}',
         slot = 'Slot no. (1,2,3)',
         current_evidence = '%{value} | Drawer %{value2}',
@@ -117,7 +115,7 @@ local Translations = {
         new_call = 'New Call',
         officer_down = 'Officer %{lastname} | %{callsign} Down',
         fine_issued = 'Fine has been issued to offender succesfully',
-        received_fine = 'State Debt Recovery has automatically recovered the fines owed...',
+        received_fine = 'State Debt Recovery has automatically recovered the fines owed...'
     },
     evidence = {
         red_hands = 'Red hands',
@@ -189,14 +187,6 @@ local Translations = {
         remove_object = 'Removing object..',
         impound = 'Impounding Vehicle..',
     },
-    target = {
-        sign_in = 'Sign In/Out service',
-        open_personal_stash = 'Open Personal Stash',
-        open_trash = 'Open Trash',
-        open_fingerprint = 'Open Fingerprint',
-        open_armory = 'Open Armory',
-        open_evidence_stash = 'Open Evidence Stash',
-    }
 }
 
 Lang = Lang or Locale:new({
